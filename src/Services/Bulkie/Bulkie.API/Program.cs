5c080289d11eae4b184b91f7243d22fdce0d7f20
jbw <hi@jbw.codes>
2021-08-23 12:22:39 +0100

    add environment variables

--- src/Services/Bulkie/Bulkie.API/Program.cs
+++ src/Services/Bulkie/Bulkie.API/Program.cs
@@ -1,5 +1,6 @@
 using Bulkie.API.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace Bulkie.API
@@ -17,6 +18,10 @@ namespace Bulkie.API
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder.AddEnvironmentVariables();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- src/Services/Bulkie/BulkieFileProcessor.API/Program.cs
+++ src/Services/Bulkie/BulkieFileProcessor.API/Program.cs
@@ -1,11 +1,12 @@
 using BulkieFileProcessor.API.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace BulkieFileProcessor.API
 {
     public class Program
-    {
+    {           
         public static void Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
@@ -17,6 +18,10 @@ namespace BulkieFileProcessor.API
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder.AddEnvironmentVariables();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
